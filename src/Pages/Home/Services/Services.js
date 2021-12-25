320633b665cd167caf5085cdebd20eb60dbf0a0e
rafaswe <mahiya35-2706@diu.edu.bd>
2021-12-25 15:08:08 +0600

    Design

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,14 +3,15 @@ import { Button, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import birth from '../../../Assets/birthCer.png';
 import nid from '../../../Assets/nid.jpg';
-
+import passport from '../../../Assets/passportCertificate.jpg'
+import './Services.css'
 const Services = () => {
     return (
         <Container className='my-5'>
             <h2 className='primary-color text-center my-5'>Our Services</h2>
             <div className="row row-cols-1 row-cols-md-3 g-4">
-                <div className="col">
-                    <div className="card h-100">
+                <div className="col ">
+                    <div className="card h-100 pt-3 custom-card">
                         <img src={birth} className="card-img-top w-50 mx-auto" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title text-center primary-color">Birth Certificate</h5>
@@ -22,7 +23,7 @@ const Services = () => {
                     </div>
                 </div>
                 <div className="col">
-                    <div className="card h-100">
+                    <div className="card h-100 custom-card pt-3">
                         <img src={nid} className="card-img-top w-50 mx-auto" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title text-center primary-color">NID CARD</h5>
@@ -34,11 +35,11 @@ const Services = () => {
                     </div>
                 </div>
                 <div className="col">
-                    <div className="card h-100">
-                        <img src="..." className="card-img-top" alt="..." />
+                    <div className="card h-100 pt-3 custom-card">
+                        <img src={passport} className="card-img-top w-50 mx-auto" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title text-center primary-color">Passport</h5>
-                            <p className="card-text text-muted">Heavy user </p>
+                            <p className="card-text text-muted">Heavy user info is not mandatory for apply. It is very flexible to apply, user friendly and also hassle free. </p>
                         </div>
                         <div className="card-footer">
                             <Button as={NavLink} to="/" variant='success w-100'>Apply Now</Button>
